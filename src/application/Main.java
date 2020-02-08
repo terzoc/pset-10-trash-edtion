570dd22be7c73fbc4c0b21b32907e9abdacd54ec
terzoc <doctort123@gmail.com>
2020-02-08 11:09:55 -0500

    java fx stuff

---
+++ src/application/Main.java
@@ -0,0 +1,33 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Pane p = new Pane();
+			Scene scene = new Scene(p,400,400);
+			Button b = new Button();
+			b.setLayoutX(100);
+			b.setLayoutY(100);
+			b.setText("Hello!");
+			p.getChildren().add(b);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
