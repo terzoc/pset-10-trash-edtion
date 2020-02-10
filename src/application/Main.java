140e834061be7ad666f2325dcee84ea41d4b13d5
terzoc <doctort123@gmail.com>
2020-02-10 16:51:29 -0500

    we counting boys

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,29 +1,54 @@
 package application;
 	
+
+
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 
 
-public class Main extends Application {
+public class Main extends Application implements EventHandler<ActionEvent> {
+	
+	Button b;
+	Button b2;
+	int count  = 0;
+	
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			Pane p = new Pane();
-			Scene scene = new Scene(p,400,400);
-			Button b = new Button();
-			b.setLayoutX(100);
-			b.setLayoutY(100);
-			b.setText("Hello!");
+	public void start(Stage primaryStage) throws Exception {
+			primaryStage.setTitle("Dictionary");
+			StackPane p = new StackPane();
+			Scene scene = new Scene(p,300,250);
+			b = new Button();
+			b2 = new Button("Reset");
+			b.setText(Integer.toString(count));
+			b.setOnAction(this);
+			b2.setOnAction(this);
+			b2.setTranslateY(30);
+			b.setMaxWidth(75);
+			b2.setMaxWidth(75);
 			p.getChildren().add(b);
+			p.getChildren().add(b2);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
+			primaryStage.show(); 
+
+	}
+	
+	public void handle(ActionEvent event) {
+		if(event.getSource() == b) {
+			count++;
+			b.setText(Integer.toString(count));
+		}
+		
+		if(event.getSource() == b2) {
+			count=0;
+			b.setText(Integer.toString(count));
 		}
 	}
 	
